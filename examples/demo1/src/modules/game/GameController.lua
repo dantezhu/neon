4e6deafd33ccbb878b1fd3f069c41d8f64b9815b
dantezhu <dantezhu@qq.com>
2014-11-09 02:23:29 +0800

    getViewClass

--- examples/demo1/src/modules/game/GameController.lua
+++ examples/demo1/src/modules/game/GameController.lua
@@ -1,14 +1,14 @@
 
 require("neon.init")
 -- local GameView = neon.import(".GameView")
-local GameView = require("modules.game.GameView")
+
 
 local M = neon.class("GameController", function (app)
 	return neon.Controller.new(app)
 end)
 
 function M:getViewClass()
-    return GameView
+    return require("modules.game.GameView")
 end
 
 return M
