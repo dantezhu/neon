5cf9dbf21cbf991f63e12f9e8e606d30fb245f98
dantezhu <dantezhu@qq.com>
2014-11-10 14:40:06 +0800

    空格

--- examples/demo1/src/modules/game/Controller.lua
+++ examples/demo1/src/modules/game/Controller.lua
@@ -8,10 +8,10 @@ M.viewClass = neon.import(".View")
 
 function M:onCreate()
     neon.loge("onCreate")
-	neon.events:addHandler(EVT.show_game, function (name, from)
-	   print(name, from)
-	   self:show()
-	end, nil)
+    neon.events:addHandler(EVT.show_game, function (name, from)
+       print(name, from)
+       self:show()
+    end, nil)
 end
 
 function M:onCreateView()
