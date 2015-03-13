8ee42337d2007ad311cfdd954a2ec4994dace7b1
dantezhu <dantezhu@qq.com>
2015-03-13 11:39:16 +0800

    改名

--- examples/demo1/src/GameApp.lua
+++ examples/demo1/src/GameApp.lua
@@ -6,8 +6,8 @@ local M = neon.class("GameApp", neon.Neon)
 function M:onCreate()
     neon.logger:setLevel(neon.Log.WARN)
 
-    self:registerView(require("views.Game"))
-    self:registerView(require("views.Loading"))
+    self:registerView(require("views.GameView"))
+    self:registerView(require("views.LoadingView"))
 
     -- 只是演示，不一定要用事件来开启view，也可以直接调用
     neon.events:addHandler(EVT.show_loading, function (from)
